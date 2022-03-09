4a48a5a4477abf0e815d6913542376dded4483d6
Cassius-Cassity <80988165+Cassius-Cassity@users.noreply.github.com>
2022-03-08 18:22:29 -0700

    finally done

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
-// implement your server here
-// require your posts router and connect it here
+const express = require('express'); // importing express to be used in the server.js file
+const postsRouter = require('./posts/posts-router')
+const server = express(); // this invokes express
+
+server.use(express.json()); // configures the data to a json format
+
+
+
+server.use('/api/posts', postsRouter)
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    });
+}); // this is a default I believe
+
+module.exports = server 
